125c9a819e97bd6a7473642218474ea061c6d13e
Luke Karrys <lukekarrys@gmail.com>
2014-05-08 10:19:36 -0700

    tweet

--- app/main.js
+++ app/main.js
@@ -1,15 +1,19 @@
-var size = 100;
+var size = 64;
 var host = 'https://raw.githubusercontent.com/HenrikJoreteg/emoji-images/master/pngs';
 var _ = require('underscore');
 var fortune = require('../lib/emoji');
 var domready = require('domready');
 var domify = require('domify');
+var tweet = require('../lib/tweet');
 
 var emoji = fortune(3, host, size);
 var images = '<span>' + _.pluck(emoji, 'image').join('') + '</span>';
-var text = _.pluck(emoji, 'name').join(' ').replace(/:/g, '').replace(/_/g, ' ').toUpperCase();
+var text = _.pluck(emoji, 'name').join(' ').replace(/:/g, '').replace(/_/g, ' ');
 
 domready(function () {
     document.querySelector('#fortune').appendChild(domify(images));
     document.querySelector('#fortuneText').innerHTML = text;
+    document.querySelector('#tweetButton').setAttribute('data-text', 'My emoji fortune is "' + text + '"');
+    tweet(document);
 });
+
