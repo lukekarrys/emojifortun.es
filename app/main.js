2363d6f5368b8b6782871b0db4b22162fc552c13
Luke Karrys <lukekarrys@gmail.com>
2017-02-17 11:02:47 -0700

    updates

--- app/main.js
+++ app/main.js
@@ -1,70 +1,69 @@
 /* global document, ga */
-var size = 64;
-var host = '/emojis';
-var _ = require('underscore');
-_.templateSettings = {interpolate: /\{\{(.+?)\}\}/g};
-var Emoji = require('random-emoji');
-var domready = require('domready');
-var share = require('../lib/share');
-var addEvent = require('../lib/event');
-var attachFastClick = require('fastclick');
-var shareTextTmpl =  _.template('My emoji fortune is {{ characters }} ({{ text }}) —');
-var $ = function (id) { return document.querySelector('#' + id); };
-var ondeck = null;
+var size = 64
+var host = '/emojis'
+var _ = require('underscore')
+_.templateSettings = {interpolate: /\{\{(.+?)\}\}/g}
+var Emoji = require('random-emoji')
+var domready = require('domready')
+var share = require('../lib/share')
+var addEvent = require('../lib/event')
+var attachFastClick = require('fastclick')
+var shareTextTmpl = _.template('My emoji fortune is {{ characters }} ({{ text }}) —')
+var $ = function (id) { return document.querySelector('#' + id) }
+var ondeck = null
 
-
-function preload(images) {
-    _.each(images, function(src) {
-        (new Image()).src = src;
-    });
+function preload (images) {
+  _.each(images, function (src) {
+    (new window.Image()).src = src
+  })
 }
 
-function getEmoji() {
-    var emoji = Emoji.random({
-        count: 3,
-        host: host,
-        height: size
-    });
-    preload(_.pluck(emoji, 'imageSrc'));
-    var names = _.pluck(emoji, 'name');
-    var text = names.join(' ').replace(/_/g, ' ');
-    var characters = _.pluck(emoji, 'character').join(' ');
-    var shareText = shareTextTmpl({text: text, characters: characters});
-    var images = _.map(emoji, function (e) {
-        var url = 'http://emojipedia.org/' + e.character;
-        return '<a href="' + url + '" target="_blank">' + e.image + '</a>';
-    }).join('');
-    return {
-        images: '<span>' + images + '</span>',
-        share: share(shareText),
-        text: text
-    };
+function getEmoji () {
+  var emoji = Emoji.random({
+    count: 3,
+    host: host,
+    height: size
+  })
+  preload(_.pluck(emoji, 'imageSrc'))
+  var names = _.pluck(emoji, 'name')
+  var text = names.join(' ').replace(/_/g, ' ')
+  var characters = _.pluck(emoji, 'character').join(' ')
+  var shareText = shareTextTmpl({text: text, characters: characters})
+  var images = _.map(emoji, function (e) {
+    var url = 'http://emojipedia.org/' + e.character
+    return '<a href="' + url + '" target="_blank">' + e.image + '</a>'
+  }).join('')
+  return {
+    images: '<span>' + images + '</span>',
+    share: share(shareText),
+    text: text
+  }
 }
 
-function setDom(opts) {
-    $('fortune').innerHTML = opts.images;
-    $('copy').setAttribute('data-text', opts.share.copy);
-    $('fortuneText').innerHTML = opts.text;
-    $('tweetButton').setAttribute('href', opts.share.twitter);
+function setDom (opts) {
+  $('fortune').innerHTML = opts.images
+  $('copy').setAttribute('data-text', opts.share.copy)
+  $('fortuneText').innerHTML = opts.text
+  $('tweetButton').setAttribute('href', opts.share.twitter)
 }
 
-function copyit() {
-    window.prompt('Copy to clipboard: Ctrl+C, Enter', this.getAttribute('data-text'));
+function copyit () {
+  window.prompt('Copy to clipboard: Ctrl+C, Enter', this.getAttribute('data-text'))
 }
 
-function doit() {
-    setDom(ondeck ? ondeck : getEmoji());
-    ondeck = getEmoji();
+function doit () {
+  setDom(ondeck || getEmoji())
+  ondeck = getEmoji()
 }
 
 function trackit () {
-    doit();
-    ga('send', 'pageview');
+  doit()
+  ga('send', 'pageview')
 }
 
 domready(function () {
-    attachFastClick(document.body);
-    doit();
-    addEvent($('refresh'), 'click', trackit);
-    addEvent($('copy'), 'click', copyit);
-});
+  attachFastClick(document.body)
+  doit()
+  addEvent($('refresh'), 'click', trackit)
+  addEvent($('copy'), 'click', copyit)
+})
--- server.js
+++ server.js
@@ -1,54 +1,56 @@
-var Build = require('moonboots-static');
-var Moonboots = require('moonboots-express');
-var express = require('express');
-var jade = require('jade');
-var fs = require('fs');
-var lessitizer = require('lessitizer');
+var Build = require('moonboots-static')
+var Moonboots = require('moonboots-express')
+var express = require('express')
+var jade = require('jade')
+var fs = require('fs')
+var path = require('path')
+var lessitizer = require('lessitizer')
 
-var appPath = __dirname + '/app/';
-var devMode = process.argv.join(' ').indexOf('--dev') > -1;
-var buildMode = process.argv.join(' ').indexOf('--build') > -1;
+var appPath = path.join(__dirname, 'app')
+var devMode = process.argv.join(' ').indexOf('--dev') > -1
+var buildMode = process.argv.join(' ').indexOf('--build') > -1
 var html = function (locals) {
-    return jade.compile(fs.readFileSync(appPath + 'index.jade'))(locals);
-};
+  return jade.compile(fs.readFileSync(path.join(appPath, 'index.jade')))(locals)
+}
 var config = {
-    main: appPath + 'main.js',
-    stylesheets: [
-        appPath + 'app.css'
-    ],
-    developmentMode: devMode,
-    beforeBuildCSS: function (cb) {
-        lessitizer({
-            files: appPath + 'app.less',
-            outputDir: appPath
-        }, cb);
-    }
-};
-
+  main: path.join(appPath, 'main.js'),
+  stylesheets: [
+    path.join(appPath, 'app.css')
+  ],
+  developmentMode: devMode,
+  beforeBuildCSS: function (cb) {
+    lessitizer({
+      files: path.join(appPath, 'app.less'),
+      outputDir: appPath
+    }, cb)
+  }
+}
 
 if (buildMode) {
-    new Build({
-        moonboots: config,
-        htmlSource: function (context) {
-            return html(context);
-        },
-        directory: __dirname + '/_site',
-        public: __dirname + '/public',
-        cb: function (err) {
-            console.log(err || 'Built!');
-        }
-    });
+  // eslint-disable-next-line no-new
+  new Build({
+    moonboots: config,
+    htmlSource: function (context) {
+      return html(context)
+    },
+    directory: path.join(__dirname, '_site'),
+    public: path.join(__dirname, 'public'),
+    cb: function (err) {
+      console.log(err || 'Built!')
+    }
+  })
 } else {
-    var server = express();
-    var port = process.env.PORT || 3001;
-    server.use(express.static(__dirname + '/public'));
-    new Moonboots({
-        server: server,
-        moonboots: config,
-        render: function (req, res) {
-            res.send(html(res.locals));
-        }
-    });
-    server.listen(port);
-    console.log('Running on localhost:' + port);
+  var server = express()
+  var port = process.env.PORT || 3001
+  server.use(express.static(path.join(__dirname, 'public')))
+  // eslint-disable-next-line no-new
+  new Moonboots({
+    server: server,
+    moonboots: config,
+    render: function (req, res) {
+      res.send(html(res.locals))
+    }
+  })
+  server.listen(port)
+  console.log('Running on localhost:' + port)
 }
